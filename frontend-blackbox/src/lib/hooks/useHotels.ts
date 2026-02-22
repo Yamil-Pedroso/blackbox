0e6c303ca0c61616d07ce0b41be792484a4806d1
Yamil Pedroso <yamirovinci@gmail.com>
2026-02-22 19:26:27 +0100

    Delete some hooks

--- frontend-blackbox/src/lib/hooks/useHotels.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getHotels } from "../services/hotel.service";
-
-export const useHotels = () => {
-  return useQuery({
-    queryKey: ["hotels"],
-    queryFn: getHotels,
-  });
-};
